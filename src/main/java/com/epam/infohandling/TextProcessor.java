1f1ff61304dc81df1c149fb9ab19679571d376d8
TatsianaYemialyanava <tatsiana.yemialyanava@gmail.com>
2021-06-18 12:51:31 +0300

    file read path added

--- src/main/java/com/epam/infohandling/TextProcessor.java
+++ src/main/java/com/epam/infohandling/TextProcessor.java
@@ -1,13 +1,40 @@
 package com.epam.infohandling;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import com.epam.infohandling.model.Composite;
 import com.epam.infohandling.parsing.ChainBuilder;
 import com.epam.infohandling.parsing.Parser;
 
 public class TextProcessor {
 
-    public Composite parseText(String text) {
-       Parser parser = new ChainBuilder().build();
-       return parser.parse(text.trim());
-    }
+	public Composite parseText(String text) throws IOException {		
+		Parser parser = new ChainBuilder().build();
+		String textRromFile = readFile(text);
+		return parser.parse(textRromFile.trim());
+	}
+
+	private String readFile(String text) throws IOException {
+		InputStream in = this.getClass().getResourceAsStream("/" + text);
+
+		try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in))) 
+		{
+			try {
+				StringBuilder sb = new StringBuilder();
+				String line = bufferedReader.readLine();
+
+				while (line != null) {
+					sb.append(line);
+					sb.append(System.lineSeparator());
+					line = bufferedReader.readLine();
+				}
+				String everything = sb.toString();
+				return everything;
+			} finally {
+				bufferedReader.close();
+			}
+		}
+	}
 }
